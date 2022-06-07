61a2f6f7ab68df77a2346168e7f30822e168997b
Alexander Echeverry Ramirez <alexanderecheverry@outlook.es>
2022-06-06 21:58:55 -0500

    initial point

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author alexanderecheverry
+ * @version 1.0
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
